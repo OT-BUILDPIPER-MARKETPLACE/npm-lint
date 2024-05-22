5d2b15b6faa17a90c35c23cc124b1c788ea98e53
mukulmj <mukul.joshi@opstree.com>
2024-05-22 18:05:15 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -6,25 +6,25 @@ source str-functions.sh
 source file-functions.sh
 source aws-functions.sh
 
-logInfoMessage "I'll ${INSTRUCTION} for files available at [$CODE_LOCATION]"
-
 CODE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
 logInfoMessage "I'll do processing at [$CODE_LOCATION]"
 sleep  $SLEEP_DURATION
 
+logInfoMessage "I'll ${INSTRUCTION} for files available at [$CODE_LOCATION]"
+
 cd  "${CODE_LOCATION}"
 
-npm install
+# npm install
 
-# Check npm install status
-if [ $? -eq 0 ]; then
-    TASK_STATUS=0
-    logInfoMessage "Successfully installed npm"
-else
-    TASK_STATUS=1
-    logErrorMessage "Failed to install npm"
-    exit 1
-fi
+# # Check npm install status
+# if [ $? -eq 0 ]; then
+#     TASK_STATUS=0
+#     logInfoMessage "Successfully installed npm"
+# else
+#     TASK_STATUS=1
+#     logErrorMessage "Failed to install npm"
+#     exit 1
+# fi
 
 if [ -d "reports" ]; then
     true
